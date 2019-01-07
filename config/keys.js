5db0a6527f1c599e5a58485840fc9d31915fe089
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-07 19:21:48 +1100

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://jimfarrugia:oWeeGrXVN0WIBWxe@jfcluster-shard-00-00-c8qsd.mongodb.net:27017,jfcluster-shard-00-01-c8qsd.mongodb.net:27017,jfcluster-shard-00-02-c8qsd.mongodb.net:27017/test?ssl=true&replicaSet=JFCluster-shard-0&authSource=admin&retryWrites=true"
+};
