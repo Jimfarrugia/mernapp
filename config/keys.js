747949af8b3a8432b86e5249cea7ec9a7036597c
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-08 18:35:39 +1100

    finished lectures 12 and 13.  starting on 14.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://jimfarrugia:oWeeGrXVN0WIBWxe@jfcluster-shard-00-00-c8qsd.mongodb.net:27017,jfcluster-shard-00-01-c8qsd.mongodb.net:27017,jfcluster-shard-00-02-c8qsd.mongodb.net:27017/test?ssl=true&replicaSet=JFCluster-shard-0&authSource=admin&retryWrites=true"
+    "mongodb://jimfarrugia:oWeeGrXVN0WIBWxe@jfcluster-shard-00-00-c8qsd.mongodb.net:27017,jfcluster-shard-00-01-c8qsd.mongodb.net:27017,jfcluster-shard-00-02-c8qsd.mongodb.net:27017/test?ssl=true&replicaSet=JFCluster-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -21,16 +22,20 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hellowee"));
+// Passport middleware
+app.use(passport.initialize());
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Passport config
+require("./config/passport")(passport);
 
 // User Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // npm run server for nodemon
 // up to lecture #8
