532b093b65c1e9ac79a094fe3aaa7bb252c49782
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-08 20:01:48 +1100

    finished lecture 15 and Section 3

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // @route   GET api/posts/test
-// @desc    Tests posts route
+// @desc    Tests post route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "posts works" }));
+router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
 module.exports = router;
