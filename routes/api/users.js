a67d229a9bedde47d92d8be4a3eb76a85db189c0
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-08 19:18:04 +1100

    finished lecture 14

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load user model
 const User = require("../../models/Users");
 
@@ -18,6 +21,13 @@ router.get("/test", (req, res) => res.json({ msg: "users works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body); // def
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res
